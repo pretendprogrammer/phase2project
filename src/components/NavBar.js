3a31590513b586fa23f10f25a7b5a0278396f6a8
Yosef <shmuli5770@gmail.com>
2021-05-14 09:42:34 -0400

    added condition error message for not loggd in users

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Menu, Message } from 'semantic-ui-react'
 import ReactDOM from 'react-dom';
 /* Add NavLink to importer */
 import { BrowserRouter as Router, Route, NavLink, Link, useHistory } from 'react-router-dom';
@@ -7,43 +7,59 @@ import { BrowserRouter as Router, Route, NavLink, Link, useHistory } from 'react
 const NavBar = (props) => {
 
     const history = useHistory()
+    const [loginPrompt, setLoginPrompt] = useState(false)
 
     return (
-        <Menu>
-                <select onChange={(e) => props.setFilter(e.target.value)}>
-                    <option value="">No Filter</option>
-                    <option value="Dating">Dating</option>
-                    <option value="Games">Games</option>
-                    <option value="Multimedia">Multimedia</option>
-                    <option value="Finance">Finance</option>
-                </select>
-            
-            <Menu.Item
-                disabled={props.username === ''}
-                name='addProgramButton'
-                onClick={() => history.push("/addNewProgram")}
-            >
-                Add Program
-            </Menu.Item>
-            <Menu.Item
-                disabled={props.username === ''}
-                name='addComparisonButton'
-                onClick={() => history.push("/addNewComparison")}
-            >
-                Add Comparison
-            </Menu.Item>
-            <Menu.Item
-                name='clearSelection'
-                onClick={() => props.clearSelections()}>
-                Clear Selections
-            </Menu.Item>
-            <Menu.Item
-                name='userLoginButton'
-                onClick={() => {props.username === '' ? history.push("/logIn") : props.setUser({username: '', userId: null})}}
-            >
-                {props.username === '' ? 'Login' : 'Log Out'}
-            </Menu.Item>
-        </Menu>
+        <div style={{paddingBottom: '1%' }}>
+            <Menu>
+                    <select onChange={(e) => props.setFilter(e.target.value)}>
+                        <option value="">No Filter</option>
+                        <option value="Dating">Dating</option>
+                        <option value="Games">Games</option>
+                        <option value="Multimedia">Multimedia</option>
+                        <option value="Finance">Finance</option>
+                    </select>
+                
+                <Menu.Item
+                    name='addProgramButton'
+                    onClick={() => {
+                        if (props.username !== '') {
+                            history.push('/addNewProgram')
+                        } else {
+                            setLoginPrompt(true)
+                            setTimeout(() => setLoginPrompt(false), 2000)
+                        }
+                    }}
+                >
+                    Add Program
+                </Menu.Item>
+                <Menu.Item
+                    name='addComparisonButton'
+                    onClick={() => {
+                        if (props.username !== '') {
+                            history.push('/addNewComparison')
+                        } else {
+                            setLoginPrompt(true)
+                            setTimeout(() => setLoginPrompt(false), 2000)
+                        }
+                    }}
+                >
+                    Add Comparison
+                </Menu.Item>
+                <Menu.Item
+                    name='clearSelection'
+                    onClick={() => props.clearSelections()}>
+                    Clear Selections
+                </Menu.Item>
+                <Menu.Item
+                    name='userLoginButton'
+                    onClick={() => {props.username === '' ? history.push("/logIn") : props.setUser({username: '', userId: null})}}
+                >
+                    {props.username === '' ? 'Login' : 'Log Out'}
+                </Menu.Item>
+            </Menu>
+            {loginPrompt ? <Message header='Please log in' content='This feature is only available to registered users. Please log in and try again.'/> : null}
+        </div>
     )
 }
 
